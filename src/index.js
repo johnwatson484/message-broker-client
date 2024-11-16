01e4f2747eae0416021c69f13456757b4ce90201
John Watson <john.watson1@defra.gov.uk>
2024-11-16 20:28:44 +0000

    Refactor

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import { ServiceBusClient } from '@azure/service-bus'
+import { sendToQueue, receiveFromQueue } from './queue.js'
+import { sendToTopic, receiveFromSubscription } from './topic.js'
+
+const client = new ServiceBusClient('Endpoint=sb://default.default.default.localhost;SharedAccessKeyName=1234;SharedAccessKey=password;UseDevelopmentEmulator=true')
+
+await sendToQueue(client, { body: 'Hello World from Queue' })
+const queueMessage = await receiveFromQueue(client)
+console.log(queueMessage)
+
+await sendToTopic(client, { body: 'Hello World from Topic' })
+const subscriptionMessage = await receiveFromSubscription(client)
+console.log(subscriptionMessage)
+
+await client.close()
+
+
