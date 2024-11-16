1b782128cec0f089699f68cfd40da32aa5abdc1b
John Watson <john.watson1@defra.gov.uk>
2024-11-16 20:35:06 +0000

    Refactor service bus client initialization for improved readability

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import { ServiceBusClient } from '@azure/service-bus'
 import { sendToQueue, receiveFromQueue } from './queue.js'
 import { sendToTopic, receiveFromSubscription } from './topic.js'
 
-const client = new ServiceBusClient('Endpoint=sb://default.default.default.localhost;SharedAccessKeyName=anything;SharedAccessKey=anything;UseDevelopmentEmulator=true')
+const connectionString = 'Endpoint=sb://default.default.default.localhost;SharedAccessKeyName=anything;SharedAccessKey=anything;UseDevelopmentEmulator=true'
+const client = new ServiceBusClient(connectionString)
 
 await sendToQueue(client, { body: 'Hello World from Queue' })
 const queueMessage = await receiveFromQueue(client)
